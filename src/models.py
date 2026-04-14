566d38a4563913d6c85da9ca3376014edd28d4af
Jim Howard <jh.xsnrg@gmail.com>
2026-04-13 21:44:20 -0500

    update requirements.txt and fix typo in models.py

--- src/models.py
+++ src/models.py
@@ -33,4 +33,3 @@ class DXDataSummary(BaseModel):
     last_refresh: datetime = Field(default_factory=lambda: datetime.now(timezone.utc).replace(tzinfo=timezone.utc))
     data_sources: list[str]
     stations: list[DXStation]
-    
